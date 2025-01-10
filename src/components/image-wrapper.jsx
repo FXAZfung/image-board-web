fbce8344c387a807dcfd23b6d94dc56fc8c1627c
佛系阿宅 <3374918255@qq.com>
2025-01-10 22:58:40 +0800

    feat:修改图片预览和详情页图片质量

--- src/components/image-wrapper.jsx
+++ src/components/image-wrapper.jsx
@@ -1,21 +1,21 @@
 import Image from "next/image";
 import {Controlled as ControlledZoom} from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css';
-import {useCallback, useEffect, useLayoutEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {Button} from "@/components/ui/button";
+import Loader from "@/components/loader";
 
 export default function ImageWrapper({image}) {
-    const [quality, setQuality] = useState(1);
+    const [quality, setQuality] = useState(5);
     const [isZoomed, setIsZoomed] = useState(false);
 
     const handleZoomChange = useCallback(shouldZoom => {
         setIsZoomed(shouldZoom);
     }, []);
 
-    const resolveImage = (image) => {
-        return "http://localhost:4536/api/public/images/" + image.file_name;
+    const resolveImage = (url) => {
+        return url ? "http://localhost:4536/api/public/images/" + url : null;
     }
-
 
     useEffect(() => {
         if (isZoomed) {
@@ -28,7 +28,9 @@ export default function ImageWrapper({image}) {
     return (
         <div>
             <ControlledZoom isZoomed={isZoomed} ZoomContent={CustomZoomContent} onZoomChange={handleZoomChange}>
-                <Image quality={quality} className="p-1" key={image.id} src={resolveImage(image)} width={image.width}
+                <Image loading="lazy" quality={quality} className="p-1" key={image.id}
+                       src={resolveImage(image.file_name)}
+                       width={image.width}
                        height={image.height}
                        alt={image.file_name}
                 />
@@ -42,16 +44,6 @@ const CustomZoomContent = ({
                                modalState,   // current state of the zoom modal: UNLOADED, LOADING, LOADED, UNLOADING
                                img,          // your image, prepped for zooming
                            }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    useLayoutEffect(() => {
-        if (modalState === 'LOADED') {
-            setIsModalOpen(true)
-        } else if (modalState === 'UNLOADING') {
-            setIsModalOpen(false)
-        }
-    }, [modalState])
-
 
     return <div className="bg-background w-full h-full">
         <Button variant="outline" asChild>
--- src/components/loading.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Loading() {
-    return (
-        <div className="fixed z-50 w-full h-screen flex items-center justify-center bg-background">
-            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-t-2 dark:border-gray-100 border-gray-900"></div>
-        </div>
-    )
-}
